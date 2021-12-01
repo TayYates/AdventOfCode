9add6ef166664df1e85fd6ff087d8c168e00f73a
taylorayates <taylor.yates@ssaihq.com>
2021-12-01 14:40:37 -0500

    separated data and scripts

--- 2021/day1.py
+++
@@ -1,7 +0,0 @@
-depths = []
-with open('./day1.txt', 'r') as txt:
-    for line in txt:
-        depths.append(int(line))
-
-print(sum(a < b for a, b in zip(depths, depths[1:])))
-print(sum(a < b for a, b in zip(depths, depths[3:])))
