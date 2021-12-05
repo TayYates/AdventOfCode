740c95c29132f5fc3d73dee1e752d55dfb8ddd43
taylorayates <taylor.yates@ssaihq.com>
2021-12-05 11:56:45 -0500

    day5

---
+++ 2021/scripts/day5.py
@@ -0,0 +1,49 @@
+import numpy as np
+import re
+
+def runTest(part):
+    # build 1,000 x 1,000 array of 0s
+    seaFloor = np.zeros((1000,1000), dtype=int)
+
+    # transform each line into a list of points [minX, minY, maxX, maxY], store these in commands
+    commands = []
+    with open('../data/day5.txt', 'r') as txt:
+        for line in txt:
+            line = re.split(',| -> ', line.strip())
+            command = [
+                int(line[0]),
+                int(line[1]),
+                int(line[2]),
+                int(line[3]),
+            ]
+            commands.append(command)
+
+    # for each command list
+    for command in commands:
+        Xmin, Ymin, Xmax, Ymax = min(command[0], command[2]), min(command[1], command[3]), max(command[0], command[2]), max(command[1], command[3])
+        # if Y is constant
+        if Ymin == Ymax:
+            # +1 onto grid while crawling from minX to maxX
+            seaFloor[Ymin, Xmin:(Xmax+1)] += 1
+        # if X is constant
+        elif Xmin == Xmax:
+            # repeat for minY to maxY
+            seaFloor[Ymin:(Ymax+1), Xmin] += 1
+        # if diagonal. Only run for part2 answer
+        elif part == 'part 2':
+            # build variables for magnitude and direction
+            deltaX = command[2] - command[0]
+            deltaY = command[3] - command[1]
+            Xdirection = deltaX / abs(deltaX)
+            Ydirection = deltaY / abs(deltaY)
+            # give this for loop a starting place, a magnitude, and a direction and let it add 1 to each point
+            for i in range(0, max(abs(deltaX), abs(deltaY)) + 1):
+                Yposition = int(command[1] + (i * Ydirection))
+                Xposition = int(command[0] + (i * Xdirection))
+                seaFloor[Yposition][Xposition] += 1
+
+    # answer
+    print(np.count_nonzero(seaFloor > 1))
+
+runTest('part 1')
+runTest('part 2')
