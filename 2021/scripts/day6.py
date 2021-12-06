e3b039a1f1f6f85de48d42b40cc1df56286c9d4c
taylorayates <taylor.ali.yates@gmail.com>
2021-12-06 18:00:06 -0500

    day6

---
+++ 2021/scripts/day6.py
@@ -0,0 +1,23 @@
+startingPop = [3,1,4,2,1,1,1,1,1,1,1,4,1,4,1,2,1,1,2,1,3,4,5,1,1,4,1,3,3,1,1,1,1,3,3,1,3,3,1,5,5,1,1,3,1,1,2,1,1,1,3,1,4,3,2,1,4,3,3,1,1,1,1,5,1,4,1,1,1,4,1,4,4,1,5,1,1,4,5,1,1,2,1,1,1,4,1,2,1,1,1,1,1,1,5,1,3,1,1,4,4,1,1,5,1,2,1,1,1,1,5,1,3,1,1,1,2,2,1,4,1,3,1,4,1,2,1,1,1,1,1,3,2,5,4,4,1,3,2,1,4,1,3,1,1,1,2,1,1,5,1,2,1,1,1,2,1,4,3,1,1,1,4,1,1,1,1,1,2,2,1,1,5,1,1,3,1,2,5,5,1,4,1,1,1,1,1,2,1,1,1,1,4,5,1,1,1,1,1,1,1,1,1,3,4,4,1,1,4,1,3,4,1,5,4,2,5,1,2,1,1,1,1,1,1,4,3,2,1,1,3,2,5,2,5,5,1,3,1,2,1,1,1,1,1,1,1,1,1,3,1,1,1,3,1,4,1,4,2,1,3,4,1,1,1,2,3,1,1,1,4,1,2,5,1,2,1,5,1,1,2,1,2,1,1,1,1,4,3,4,1,5,5,4,1,1,5,2,1,3]
+
+def condition(x, i):
+    return x == i
+
+# build empty list of fish populations sorted by age
+runningPop = [0] * 9
+for i in range(1, 6):
+    runningPop[i] = (sum(condition(x, i) for x in startingPop))
+
+# go through 256 days, treating each age group as a giant unit
+for j in range(0, 256):
+    # count newborns
+    newBabies = runningPop[0]
+    # age all the fish
+    runningPop.pop(0)
+    runningPop.append(0)
+    # add newborns to 6 and 8
+    runningPop[6] += newBabies
+    runningPop[8] += newBabies
+
+# count all fish
+print(sum(runningPop))
