2bb9c622b231730c62bcaf692406b6cdf1f9c812
taylorayates <taylor.ali.yates@gmail.com>
2021-12-10 22:15:33 -0500

    consolidated part 1 and 2

--- 2021/scripts/day10.py
+++ 2021/scripts/day10.py
@@ -5,15 +5,16 @@ with open('../data/day10.txt', 'r') as file:
     for line in file.read().splitlines():
         lines.append(line)
 
-#####################################
-# part 1
-
 openers = ['(', '[', '{', '<']
 closers = [')', ']', '}', '>']
-scores = {')': 3, ']': 57, '}': 1197, '>': 25137}
+scores1 = {')': 3, ']': 57, '}': 1197, '>': 25137}
+scores2 = {'(': 1, '[': 2, '{': 3, '<': 4}
 
-score = 0
+score1 = 0
+lineScores = []
 for line in lines:
+    badLine = False
+    score2 = 0
     # for each line, build a list called runners of all the openers currently in play
     runner = []
     for char in list(line):
@@ -25,44 +26,17 @@ for line in lines:
             runner.pop()
         else:
             # if the closer doesn't match the opener, add the score to the running total
-            score += scores[char]
-            break
-# answer part 1
-print(score)
-
-#####################################
-# part 2
-
-openers = ['(', '[', '{', '<']
-closers = [')', ']', '}', '>']
-pairs = {'(': ')', '[': ']', '{': '}', '<': '>'}
-scores = {'(': 1, '[': 2, '{': 3, '<': 4}
-
-lineScores = []
-for line in lines:
-    badLine = False
-    # for each line, build a list called runners of all the openers currently in play
-    score = 0
-    runner = []
-    for char in list(line):
-        # build counts of all items
-        if char in openers:
-            # add opener to runner
-            runner.append(char)
-        # when a closer appears, if the closer matches the most recent opener, annihilate that opener
-        elif char in closers and char in closers[openers.index(runner[-1])]:
-            runner.pop()
-        # else, this is a corrupted line, don't count the score here
-        else:
+            score1 += scores1[char]
             badLine = True
             break
-    # run through the runner list, popping items and adding to the score until you've run through them all
-    while len(runner) > 0:
-        score *= 5
-        score += scores[runner[-1]]
-        runner.pop()
-    # only append non corrupted lines
     if badLine == False:
-        lineScores.append(score)
+        # go through the runner list, popping items and adding to the score until you've run through them all
+        while len(runner) > 0:
+            score2 *= 5
+            score2 += scores2[runner[-1]]
+            runner.pop()
+        lineScores.append(score2)
+# answer part 1
+print(score1)
 # answer part 2
 print(median(lineScores))
